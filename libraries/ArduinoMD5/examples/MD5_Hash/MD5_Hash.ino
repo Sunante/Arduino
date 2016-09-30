40ef42bbd9ded9eaf89b9082694109a795d50cdc
i-esan.com <sunan_te@hotmail.com>
2016-09-30 07:22:37 +0700

    add new library
    work for me

---
+++ libraries/ArduinoMD5/examples/MD5_Hash/MD5_Hash.ino
@@ -0,0 +1,32 @@
+#include <MD5.h>
+/*
+This is en example of how to use my MD5 library. It provides two
+easy-to-use methods, one for generating the MD5 hash, and the second
+one to generate the hex encoding of the hash, which is frequently used.
+*/
+void setup()
+{
+  //initialize serial
+  Serial.begin(9600);
+  //give it a second
+  //delay(1000);
+  //generate the MD5 hash for our string
+  unsigned char* hash=MD5::make_hash("hello world");
+  //generate the digest (hex encoding) of our hash
+  char *md5str = MD5::make_digest(hash, 16);
+  free(hash);
+  //print it on our serial monitor
+  Serial.println(md5str);
+  //Give the Memory back to the System if you run the md5 Hash generation in a loop
+      if (strcmp (md5str,"5eb63bbbe01eeed093cb22bb8f5acdc3") == 0){
+      //if(md5str == "5eb63bbbe01eeed093cb22bb8f5acdc3"){
+          Serial.println("Verify MD5 : True");
+      }else{
+          Serial.println("Verify MD5 : False");
+      }
+  free(md5str);
+}
+
+void loop()
+{
+}
